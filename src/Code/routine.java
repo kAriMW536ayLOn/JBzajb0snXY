c0a70d8967c56bbf66401d95aaa70ff404c0f3c6
kAriMW536ayLOn <telefax_saintly.0c@icloud.com>
2025-01-01 04:02:28 +0100

    改进了 validate_user 函数的实现

---
+++ src/Code/routine.java
@@ -0,0 +1,15 @@
+
+package Code;
+
+
+public class routine {
+    private String courseCode;
+    private String addteachername;
+    private String time;
+    
+    public void setRoutine(){
+        
+    }
+            
+    
+}
